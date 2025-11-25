11bf84428c477720d88556f72be0326f8a8d75a8
lh-rubz <habuelrub@gmail.com>
2025-11-25 23:48:00 +0300

    feat: Update default OLLAMA_MODEL to gemma3:27b

--- config.py
+++ config.py
@@ -60,7 +60,7 @@ class Config:
     
     # Ollama configuration
     OLLAMA_BASE_URL = os.getenv('OLLAMA_BASE_URL', 'http://localhost:11434/v1')
-    OLLAMA_MODEL = os.getenv('OLLAMA_MODEL', 'gemma3:12b')
+    OLLAMA_MODEL = os.getenv('OLLAMA_MODEL', 'gemma3:27b')
 
 
 def send_brevo_email(recipient_email, subject, html_content):
